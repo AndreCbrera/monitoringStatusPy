dfa08d8e6f531d5ff76c005517d32e399defd6ab
dacq <den282f@gmail.com>
2021-10-11 12:21:44 +0200

    Documentando funciones y agregando tiempos para los logs

--- ping.py
+++ ping.py
@@ -11,6 +11,7 @@ hostList = open("./hostList.txt", "r")
 
 ips = hostList.readlines()
 
+#Ping fcuntion
 def pingCheck(ips):
     for ip in ips:
         response = subprocess.Popen(["ping", "-c", "1", ip.strip()],
@@ -25,17 +26,32 @@ def pingCheck(ips):
             status = ip.rstrip() + " Offline"    
         print(status)
 
+#Calculate downtime and uptime of connections
 def calculateTime(start, stop):
-    diference = stop -start
+    diference = stop - start
     seconds = float(str(diference.totalSeconds()))
     return str(datetime.timedelta(seconds=seconds)).split(".")[0]
 
+#Start time function
+def monitorStartCalculeTime():
+    
+    monitorStartTime = datetime.datetime.now()
+
+    monitorDateTime = "Ping monitor started at: " + \
+        str(monitorStartTime).split(".")[0]
+
+    print(monitorDateTime)
+ 
+#main function
 def main():
-    #print(ips)
-    #pingCheck(ips)
+    #Monitoring up datetime
+    monitorStartCalculeTime()
+    
+    #Infinite loop to check connections
     while True:
         if pingCheck(ips):
             time.sleep(5)
+        #if false, the script will show a fail mesage 
         else:
             downTime = datetime.datetime.now()
             failMsg = "disconnectetd at: " + str(downTime).split(".")[0]
@@ -50,21 +66,21 @@ def main():
 
             upTime = datetime.datetime.now()
 
+            #will be execute after while is true 
             upTimeMsg = "Connected again: " + str(upTime).split(".")[0]
 
             downTime = calculateTime(downTime, upTime)
-            
+            #calling time calculating a downtime
             unavailablityTime = "Connection was  unavailablity for : " + downTime
 
             print(upTimeMsg)
             print(unavailablityTime)
-
+            
+            # if de ping fails we log errors
             with open(FILE, "a") as file:
 
                 file.write(upTimeMsg + "\n")
                 file.write(unavailablityTime + "\n")
-
-
 
 if __name__ == "__main__":
     main()
